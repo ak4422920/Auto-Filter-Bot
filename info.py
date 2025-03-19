3a5f43125566ac5cf11450b080c5c8043f2be4aa
ak4422920 <ak4422920@gmail.com>
2025-03-19 20:24:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/+U9ABfC7hu1EyZjU1')
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/AKMovieBotz')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Cineoriginals')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/xxxpornrxx')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/akhowtoopenlinks")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/akhowtoopenlinks/45")
 
